8678df5df6debdb68b98d76a8950c224a9621926
Chandrima Pramanick <82940042+Chandrima1@users.noreply.github.com>
2023-04-03 14:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ movies['title'].values)
 
 if st.button('Recommend'):
     names,posters = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5= st.beta_columns(5)
+    col1, col2, col3, col4, col5= st.columns(5)
     with col1:
         st.text(names[0])
         st.image(posters[0])
